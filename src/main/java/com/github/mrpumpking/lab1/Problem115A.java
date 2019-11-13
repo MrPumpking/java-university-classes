9c87f016176674e31d553c612233f9f4c8947b88
Michał Bar <logout400@gmail.com>
2019-11-13 19:14:05 +0100

    feat: implement solution for Problem115A

--- src/main/java/com/github/mrpumpking/lab1/Problem115A.java
+++ src/main/java/com/github/mrpumpking/lab1/Problem115A.java
@@ -9,7 +9,30 @@ public class Problem115A {
   }
 
   public Problem115A() {
-    Scanner in = new Scanner(System.in);
-    int workerCount = in.nextInt();
+    Scanner scanner = new Scanner(System.in);
+    int workerCount = scanner.nextInt();
+
+    int answer = 0;
+    int count = 0;
+
+    int[] workers = new int[workerCount + 9];
+
+    for (int i = 1; i <= workerCount; i++) {
+      workers[i] = scanner.nextInt();
+    }
+
+    for (int i = 1; i <= workerCount; i++) {
+      count = 0;
+      int worker = workers[i];
+
+      while (worker != -1) {
+        worker = workers[worker];
+        count++;
+      }
+
+      answer = Math.max(answer, count);
+    }
+
+    System.out.println(answer + 1);
   }
 }
