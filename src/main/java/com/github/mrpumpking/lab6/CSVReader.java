7b3cabb26726e0ce6be1d90fd05040faf33f190e
Michał Bar <logout400@gmail.com>
2019-11-16 00:31:56 +0100

    feat: implement CSVReader

--- src/main/java/com/github/mrpumpking/lab6/CSVReader.java
+++ src/main/java/com/github/mrpumpking/lab6/CSVReader.java
@@ -2,23 +2,37 @@ package com.github.mrpumpking.lab6;
 
 import com.github.mrpumpking.lab6.exceptions.ColumnIndexOutOfBounds;
 import com.github.mrpumpking.lab6.exceptions.ColumnNotFoundException;
+import com.google.inject.internal.util.Lists;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class CSVReader {
   private String delimiter;
   private String[] current;
+  private int currentLength;
   private boolean hasHeader;
   private BufferedReader reader;
   private Map<String, Integer> columnLabelToIndex;
 
+  private static final String DEFAULT_DELIMITER = ",";
+  private static final String DEFAULT_TIME_FORMAT = "HH:mm:ss";
+  private static final String DEFAULT_DATE_FORMAT = "dd.MM.yyyy";
+  private static final String DEFAULT_DATE_TIME_FORMAT =
+      DEFAULT_TIME_FORMAT + " " + DEFAULT_DATE_FORMAT;
+  private static final String SPLIT_REGEX = "%s(?=([^\"]*\"[^\"]*\")*[^\"]*$)";
+
   public CSVReader(String filePath) throws IOException {
-    this(filePath, ",", false);
+    this(filePath, DEFAULT_DELIMITER, false);
   }
 
   public CSVReader(String filePath, String delimiter) throws IOException {
@@ -32,7 +46,7 @@ public class CSVReader {
   public CSVReader(Reader reader, String delimiter, boolean hasHeader) throws IOException {
     this.columnLabelToIndex = new HashMap<>();
     this.reader = new BufferedReader(reader);
-    this.delimiter = delimiter;
+    this.delimiter = buildDelimiterRegex(delimiter);
     this.hasHeader = hasHeader;
 
     if (hasHeader) {
@@ -48,6 +62,7 @@ public class CSVReader {
     }
 
     current = separate(line);
+    currentLength = line.length();
     return true;
   }
 
@@ -68,6 +83,110 @@ public class CSVReader {
 
   public void close() throws IOException {
     reader.close();
+  }
+
+  public int getInt(int index) throws ColumnIndexOutOfBounds {
+    return Integer.parseInt(get(index));
+  }
+
+  public int getInt(String column) throws ColumnNotFoundException {
+    return Integer.parseInt(get(column));
+  }
+
+  public double getDouble(int index) throws ColumnIndexOutOfBounds {
+    return Double.parseDouble(get(index));
+  }
+
+  public double getDouble(String column) throws ColumnNotFoundException {
+    return Double.parseDouble(get(column));
+  }
+
+  public long getLong(int index) throws ColumnIndexOutOfBounds {
+    return Long.parseLong(get(index));
+  }
+
+  public long getLong(String column) throws ColumnNotFoundException {
+    return Long.parseLong(get(column));
+  }
+
+  public LocalTime getTime(int index) throws ColumnIndexOutOfBounds {
+    return getTime(index, DEFAULT_TIME_FORMAT);
+  }
+
+  public LocalTime getTime(int index, String format) throws ColumnIndexOutOfBounds {
+    return LocalTime.parse(get(index), DateTimeFormatter.ofPattern(format));
+  }
+
+  public LocalTime getTime(String column) throws ColumnNotFoundException {
+    return getTime(column, DEFAULT_TIME_FORMAT);
+  }
+
+  public LocalTime getTime(String column, String format) throws ColumnNotFoundException {
+    return LocalTime.parse(get(column), DateTimeFormatter.ofPattern(format));
+  }
+
+  public LocalDate getDate(int index) throws ColumnIndexOutOfBounds {
+    return getDate(index, DEFAULT_DATE_FORMAT);
+  }
+
+  public LocalDate getDate(int index, String format) throws ColumnIndexOutOfBounds {
+    return LocalDate.parse(get(index), DateTimeFormatter.ofPattern(format));
+  }
+
+  public LocalDate getDate(String column) throws ColumnNotFoundException {
+    return getDate(column, DEFAULT_DATE_FORMAT);
+  }
+
+  public LocalDate getDate(String column, String format) throws ColumnNotFoundException {
+    return LocalDate.parse(get(column), DateTimeFormatter.ofPattern(format));
+  }
+
+  public LocalDateTime getDateTime(int index) throws ColumnIndexOutOfBounds {
+    return getDateTime(index, DEFAULT_DATE_TIME_FORMAT);
+  }
+
+  public LocalDateTime getDateTime(int index, String format) throws ColumnIndexOutOfBounds {
+    return LocalDateTime.parse(get(index), DateTimeFormatter.ofPattern(format));
+  }
+
+  public LocalDateTime getDateTime(String column) throws ColumnNotFoundException {
+    return getDateTime(column, DEFAULT_DATE_TIME_FORMAT);
+  }
+
+  public LocalDateTime getDateTime(String column, String format) throws ColumnNotFoundException {
+    return LocalDateTime.parse(get(column), DateTimeFormatter.ofPattern(format));
+  }
+
+  public List<String> getColumnLabels() {
+    return Lists.newArrayList(columnLabelToIndex.keySet());
+  }
+
+  public int getRecordLength() {
+    return currentLength;
+  }
+
+  public boolean isMissing(int index) {
+    try {
+      return get(index).isEmpty();
+    } catch (ColumnIndexOutOfBounds exception) {
+      return true;
+    }
+  }
+
+  public boolean isMissing(String column) {
+    try {
+      return get(column).isEmpty();
+    } catch (ColumnNotFoundException exception) {
+      return true;
+    }
+  }
+
+  public boolean hasHeader() {
+    return hasHeader;
+  }
+
+  private String buildDelimiterRegex(String delimiter) {
+    return String.format(SPLIT_REGEX, delimiter);
   }
 
   private String[] separate(String line) {
--- src/main/java/com/github/mrpumpking/lab6/Main.java
+++ src/main/java/com/github/mrpumpking/lab6/Main.java
@@ -1,16 +1,28 @@
 package com.github.mrpumpking.lab6;
 
+import com.github.mrpumpking.lab6.exceptions.ColumnNotFoundException;
+
 import java.io.IOException;
-import java.nio.file.Paths;
 
 public class Main {
 
   public static void main(String[] args) {
+    new Main();
+  }
+
+  public Main() {
+    String filePath = getClass().getResource("/with-header.csv").getPath();
+
     try {
-      CSVReader reader =
-          new CSVReader(
-              Paths.get("src", "main", "resources", "with-header.csv").toString(), ";", true);
+      CSVReader reader = new CSVReader(filePath, ";", true);
+      reader.next();
+
+      System.out.println(reader.getColumnLabels());
+      System.out.println(reader.get("nazwisko"));
+
     } catch (IOException e) {
+      e.printStackTrace();
+    } catch (ColumnNotFoundException e) {
       e.printStackTrace();
     }
   }
