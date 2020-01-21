b936764f5ede630f1c713f1a33f11adcf7cde021
Michał Bar <logout400@gmail.com>
2020-01-21 15:24:13 +0100

    feat: finish

--- src/main/java/com/github/mrpumpking/kolokwium/WordCount.java
+++ src/main/java/com/github/mrpumpking/kolokwium/WordCount.java
@@ -31,10 +31,34 @@ public class WordCount {
     Set<String> shortest = null;
     Set<String> longest = null;
 
+    int median = -1;
+
+    int medianA = -1;
+    int medianB = -1;
+
+    int sum = 0;
+
     for (Map.Entry<Integer, Set<String>> entry : lengthToWords.entrySet()) {
+      sum += entry.getKey();
+
       if (i == 0) {
         shortest = entry.getValue();
         shortestLength = entry.getKey();
+      }
+
+      if (lengthToWords.size() % 2 == 0) {
+        int half = (lengthToWords.size() - 2) / 2;
+
+        if (i == half) {
+          medianA = entry.getKey();
+        } else if (i == half + 1) {
+          medianB = entry.getKey();
+        }
+
+      } else {
+        if (i == (lengthToWords.size() - 1) / 2) {
+          median = entry.getKey();
+        }
       }
 
       if (i == lengthToWords.size() - 1) {
@@ -45,11 +69,35 @@ public class WordCount {
       i++;
     }
 
+    if (medianA != -1) {
+      median = (medianA + medianB) / 2;
+    }
+
+    int avg = sum / lengthToWords.size();
+
     System.out.println("Długość najkrótszych słów: " + shortestLength);
     System.out.println(shortest);
 
     System.out.println("Długość najdłuższych słów: " + longestLength);
     System.out.println(longest);
+
+    System.out.println("Mediana: " + median);
+    System.out.println(findClosest(median));
+
+    System.out.println("Srednia: " + avg);
+    System.out.println(findClosest(avg));
+  }
+
+  private Set<String> findClosest(int key) {
+    int closest = Integer.MAX_VALUE;
+
+    for (Map.Entry<Integer, Set<String>> entry : lengthToWords.entrySet()) {
+      if (Math.abs(entry.getKey() - key) < Math.abs(entry.getKey() - closest)) {
+        closest = entry.getKey();
+      }
+    }
+
+    return lengthToWords.get(closest);
   }
 
   private void processWords() {
